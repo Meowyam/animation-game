47a3641f56a2694afa4a051aae7c0ae93be996ee
Meowyam <maryam@chilon.net>
2020-07-16 02:39:24 +0800

    Added first chord to staff

--- main.js
+++ main.js
@@ -50,6 +50,7 @@
     if (chord == 'cma7') {
       cma7()
       lightKeys(['C','E','B'])
+      drawcma7()
     } else if (chord == 'cm7') {
       cm7()
       lightKeys(['C','Dsharp','Asharp'])
@@ -68,30 +69,59 @@ const offset = 10;
 const segments = 20;
 const strokeWidth = 5;
 const strokeColor = 'black';
+const rad = 15;
 
 const canvas = document.querySelector('.squig');
 paper.setup(canvas);
 const view = paper.view;
 
-const path = new Path();
-path.strokeColor = strokeColor;
-path.strokeWidth = strokeWidth;
-path.strokeCap = 'round';
+const width = view.size.width
 
-for (let i = 0; i <= segments; i++) {
-  let width = view.size.width - (offset * 1);
-  path.add(new Point((i / segments * width + offset), view.size.height / 2));
+const path1 = new Path.Line(new Point(100,50), new Point((width - 100), 50));
+const path2 = new Path.Line(new Point(100,80), new Point((width - 100), 80));
+const path3 = new Path.Line(new Point(100,110), new Point((width - 100), 110));
+const path4 = new Path.Line(new Point(100,140), new Point((width - 100), 140));
+const path5 = new Path.Line(new Point(100,170), new Point((width - 100), 170));
+
+const paths = [path1, path2, path3, path4, path5]
+
+paths.forEach(p => {
+  p.strokeColor = strokeColor;
+  p.strokeWidth = strokeWidth;
+  p.strokeCap = 'round';
+})
+
+let notes = []
+
+let fillNotes = function(notes) {
+  notes.forEach(n => {
+    n.fillColor = strokeColor;
+  })
 }
 
-path.onFrame = (e) => {
-	for (var i = 0; i <= segments; i++) {
-    let height = 10;
-    let sinus = Math.sin(e.time * 3 + i);
-		path.segments[i].point.y = sinus * height + 25;
-    path.smooth({
-      type: 'continuous'
-    });
-	}
+const cNote = new Path.Circle({
+  center: [(width/2), 200],
+  radius: rad,
+})
+const cNoteLine = new Path.Line({
+  from: [(width/2 - 20), 200],
+  to: [(width/2 + 20), 200],
+})
+
+const eNote = new Path.Circle({
+  center: [(width/2), 170],
+  radius: rad,
+})
+
+const bNote = new Path.Circle({
+  center: [(width/2), 110],
+  radius: rad,
+})
+
+const drawcma7 = function() {
+  fillNotes([cNote, eNote, bNote])
+  cNoteLine.strokeColor = strokeColor
+  cNoteLine.strokeWidth = strokeWidth
 }
 
 paper.view.draw();
